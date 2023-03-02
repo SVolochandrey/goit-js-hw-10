e01c0458d1a2751621bd9f6ec05980639f68f917
SVolochandrey <volochan.andrey@gmail.com>
2023-03-02 16:07:32 +0200

    -

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/countries';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+const countriesListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(event){
+const findCountry = event.target.value.trim();
+
+if (!findCountry){
+dataCleaning();
+return;
+}
+else {
+fetchCountries(findCountry)
+.then(countries => {
+countries.length > 10
+? Notiflix.Notify.info(
+'Too many matches found. Please enter a more specific name.'
+)
+: onRenderCountryList(countries);
+})
+.catch(onFetchError);
+}
+}
+
+function onRenderCountryList(countries) {
+dataCleaning();
+const markup = countries
+.map(
+country => `li class ="country-item">
+<img src="${country.flags.svg}" alt="${country.name.oficial}" width = "40px" heignt = "40px">
+</img> <span> ${country.name.official} </span>
+</li>`
+)
+.join('')
+countriesListEl.innerHTML = markup;
+
+if (countries.length === 1){
+countriesListEl.classList.add('country-list__big');
+creatCountryInfo(countries)
+}
+}
+
+function creatCountryInfo(countries){
+const country = countries[0];
+countryInfoEl.innerHTML = `<p>Capital: <span>${country.capital}</span></p> 
+<p>Population: <span>${country.population}</span></p>
+<p>Languages: <span>${Object.values(country.languages)}</span></p>`
+}
+
+function onFetchError(error){
+dataCleaning();
+Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function dataCleaning(){
+countriesListEl.innerHTML = '';
+countryInfoEl.innerHTML = '';
+countriesListEl.classList.remove('country-list__big')
+}
+
